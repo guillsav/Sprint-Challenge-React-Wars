f02638bc00489beed5868ebb6398eb262f7879ce
guillsav <guillsav@gmail.com>
2019-03-22 13:51:29 -0400

    MVP done

--- starwars/src/components/CharactersList.js
+++ starwars/src/components/CharactersList.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import Character from './Character';
 
 const CharactersList = ({characters}) => {
-  return <div>{console.log(characters)}</div>;
+  return (
+    <div className="grid-card">
+      {characters.map(({name, height, mass, created, films, url}) => {
+        return (
+          <Character
+            key={created}
+            name={name}
+            height={height}
+            mass={mass}
+            films={films}
+            url={url}
+          />
+        );
+      })}
+    </div>
+  );
 };
 
 export default CharactersList;
